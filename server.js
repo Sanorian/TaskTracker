b894894dcba062d0d8849f08a541dafc7a83b820
Sanorian <krajnov.sascha@yandex.ru>
2023-09-02 18:41:47 +0400

    Add task deleting

--- server.js
+++ server.js
@@ -6,23 +6,115 @@ app.get('/login', (req, res)=>{
     res.header('Access-Control-Allow-Origin', '*');
     const username = req.query.username;
     const password = req.query.password;
-    console.log(username, password);
+    let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            reject(err.message);
+            res.send({res:'bad'});
+        }
+      });
+    db.all('SELECT * FROM users WHERE username = ? and password = ?',[username, password], (err, rows) => {
+        if (err) {
+          console.log(err);
+          res.send({res:'bad'});
+        }
+        if (rows.length==1){
+            res.send({res:'good', userid: rows[0].userid});
+        } else {
+            res.send({res:'bad'});
+        }
+        });
+    db.close( (err) => {
+        if (err) {
+            reject(err.message);
+    }
+    });
 });
 
 app.get('/registration', (req, res)=>{
     res.header('Access-Control-Allow-Origin', '*');
     const username = req.query.username;
     const password = req.query.password;
-    console.log(username, password);
-
+    let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            reject(err.message);
+            res.send({res:'bad'});
+        }
+      });
+    db.all('SELECT * FROM users', (err, rows) => {
+        if (err) {
+          console.log(err);
+        }
+        let already = false;
+        rows.forEach((user) =>{
+          if (user.username == username){
+            already = true;
+          }
+        });
+        if (already){
+          res.send({res:'already'})
+        } else {
+          newId = Number(rows[rows.length-1].userid)+1;
+          db.run(`INSERT INTO users (userid, username, password) VALUES(?, ?, ?)`, [newId, username, password], function(err) {
+            if (err) {
+              res.send({res:'bad'});
+              return console.log(err.message);
+            }
+            res.send({res:'good'});
+          });
+        }
+      });
+    
+      db.close( (err) => {
+        if (err) {
+            reject(err.message);
+        }
+      });
 });
 
 app.get('/gettasks', (req, res)=>{
     res.header('Access-Control-Allow-Origin', '*');
-    const username = req.query.username;
-    const password = req.query.password;
-    //SELECT * FROM tasks WHERE userid = (SELECT userid FROM users WHERE username = ? and password = ?),[username, password]
-    //Запрос
+    const userid = req.query.userid;
+    let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            reject(err.message);
+            res.send({res:'bad'});
+        }
+    });
+    db.all('SELECT * FROM tasks WHERE userid = ?', [userid], (err, rows) => {
+        if (err) {
+          console.log(err);
+          res.send({res:'bad'});
+        }
+        res.send(rows);
+    });
+    db.close( (err) => {
+        if (err) {
+            reject(err.message);
+        }
+    });
+});
+
+app.get('/deletetask', (req, res)=>{
+  res.header('Access-Control-Allow-Origin', '*');
+  const taskId = req.query.id;
+  let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        reject(err.message);
+        res.send({res:'bad'});
+    }
+  });
+  db.run(`DELETE FROM tasks WHERE id=?`, [taskId], function(err) {
+    if (err) {
+      res.send({res:'bad'});
+      return console.log(err.message);
+    }
+    res.send({res:'good'});
+  });
+  db.close( (err) => {
+    if (err) {
+        reject(err.message);
+}
+});
 });
 
 port=3000;
--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,21 @@ const password = ref();
 const errorPlace = ref();
 
 //Массивы задач
-const newTasks = ref([]);
-const workTasks = ref([]);
-const reviewTasks = ref([]);
-const doneTasks = ref([]);
+const newTasks = ref([{userid:0, id:0, taskcategory:'new', task:'Кажется нет доступа к серверу'}]);
+const workTasks = ref([{userid:0, id:0, taskcategory:'work', task:'Кажется нет доступа к серверу'}]);
+const reviewTasks = ref([{userid:0, id:0, taskcategory:'review', task:'Кажется нет доступа к серверу'}]);
+const doneTasks = ref([{userid:0, id:0, taskcategory:'done', task:'Кажется нет доступа к серверу'}]);
 
-function getTasks(){
-  fetch('http://192.168.31.209:3000/gettasks?username='+username.value+'&password='+password.value)
+function getTasks(userid){
+  fetch('http://192.168.31.209:3000/gettasks?userid='+userid)
     .then(response => response.json())
     .then(data => {
+      newTasks.value = [];
+      workTasks.value = [];
+      reviewTasks.value = [];
+      doneTasks.value = [];
       data.forEach(task => {
-        switch(task.category){
+        switch(task.taskcategory){
           case 'new':
             newTasks.value.push(task);
             break;
@@ -43,29 +47,66 @@ function getTasks(){
     });
 }
 function logIn(){
-  fetch('http://192.168.31.209:3000/login?username='+username.value+'&password='+password.value)
+  if (username.value=='' || password.value==''){
+    errorPlace.value = 'Вы не ввели необходимые данные';
+  } else {
+    fetch('http://192.168.31.209:3000/login?username='+username.value+'&password='+password.value)
     .then(response => response.json())
     .then(data => {
       if (data.res=='good'){
-        getTasks();
+        errorPlace.value = '';
+        getTasks(data.userid);
+        toTasks();
       } else{
         errorPlace.value = 'Возможно вы ошиблись в имени пользователя или пароле. Если у вас нет профиля, то зарегистрируйтесь'
       }
       })
     .catch(error => {
       console.log(error);
-    });
+    });  }
+
 }
 function registration(){
   fetch('http://192.168.31.209:3000/registration?username='+username.value+'&password='+password.value)
     .then(response => response.json())
     .then(data => {
       if (data.res=='good'){
+        errorPlace.value = 'Вы успешно зарегистрированы';
         getTasks();
-      } else{
-        errorPlace.value = 'Проблема на сервере. Попробуйте попозже'
+        toLogIn();
+      } else if (data.res=='already'){
+        errorPlace.value = 'Имя пользователя уже занято';
+      } else {
+        errorPlace.value = 'Проблема на сервере. Попробуйте попозже';
       }
       })
+    .catch(error => {
+      console.log(error);
+    });
+}
+function deleteTask(task){
+  fetch('http://192.168.31.209:3000/deletetask?id='+task.id)
+    .then(response => response.json())
+    .then(data => {
+      if (data.res=='bad'){
+        errorPlace.value = 'Ошибка на сервере. Не удалось удалить задачу';
+      } else {
+        switch(task.taskcategory){
+          case 'new':
+            newTasks.value = newTasks.value.filter((Task)=>task!=Task);
+            break;
+          case 'work':
+            workTasks.value = workTasks.value.filter((Task)=>task!=Task);
+            break;
+          case 'review':
+            reviewTasks.value = reviewTasks.value.filter((Task)=>task!=Task);
+            break;
+          case 'done':
+            doneTasks.value = doneTasks.value.filter((Task)=>task!=Task);
+            break;
+        }
+      }
+    })
     .catch(error => {
       console.log(error);
     });
@@ -79,47 +120,76 @@ function toLogIn(){
   isRegistration.value = false;
   isShowTasks.value = false;
   isLogin.value = true;
-
+}
+function toTasks(){
+  isLogin.value = false;
+  isRegistration.value = false;
+  isShowTasks.value = true;
 }
 </script>
 
 <template>
   <div class="allDiv">
     <div v-if="isLogin">
-      <h2>{{errorPlace}}</h2>
+      <h2 class="errorPlace">{{errorPlace}}</h2>
       <h2>Вход</h2>
       <h3>Имя пользователя</h3>
       <input v-model="username">
       <h3>Пароль</h3>
       <input v-model="password">
       <button @click="logIn()">Войти</button>    
-      <p>Еще нет аккаунта?<a @click="toRegistration()">Зарегистрироваться</a></p>
+      <p>Еще нет аккаунта? <a @click="toRegistration()">Зарегистрироваться</a></p>
     </div>
 
     <div v-if="isRegistration">
-      <h2>{{errorPlace}}</h2>
+      <h2 class="errorPlace">{{errorPlace}}</h2>
       <h2>Регистрация</h2>
       <h3>Имя пользователя</h3>
       <input v-model="username">
       <h3>Пароль</h3>
       <input v-model="password">
       <button @click="registration()">Зарегистрироваться</button>
-      <p>Уже есть аккаунт?<a @click="toLogIn">Войти</a></p>
+      <p>Уже есть аккаунт? <a @click="toLogIn">Войти</a></p>
     </div>
 
     <main v-if="isShowTasks">
-      <div v-for="newTask in newTasks" :key="newTask.id"></div>
-      <div v-for="workTask in workTasks" :key="workTask.id"></div>
-      <div v-for="reviewTask in reviewTasks" :key="reviewTask.id"></div>
-      <div v-for="doneTask in doneTasks" :key="doneTask.id"></div>
+      <h2 class="errorPlace">{{errorPlace}}</h2>
+      <div>
+        <div>
+          <textarea></textarea>
+          <button @click="addNewTask()">Добавить новую задачу</button>
+        </div>
+        <div v-for="newTask in newTasks" :key="newTask.id">
+          <p>{{newTask.task}}</p>
+          <button @click="deleteTask(newTask.id)"></button>
+        </div> 
+      </div>
+      <div v-for="workTask in workTasks" :key="workTask.id">
+        <p>{{workTask.task}}</p>
+        <button @click="deleteTask(workTask.id)"></button>
+      </div>
+      <div v-for="reviewTask in reviewTasks" :key="reviewTask.id">
+        <p>{{reviewTask.task}}</p>
+        <button @click="deleteTask(reviewTask.id)"></button>
+      </div>
+      <div v-for="doneTask in doneTasks" :key="doneTask.id">
+        <p>{{doneTask.task}}</p>
+        <button @click="deleteTask(doneTask.id)"></button>
+      </div>
     </main>
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Jost:wght@100;500&display=swap');
+p{
+  font-family: 'Didact Gothic', sans-serif;
+  font-family: 'Jost', sans-serif;
+}
 a{
   color: blue;
-  
+  font-family: 'Didact Gothic', sans-serif;
+  font-family: 'Jost', sans-serif;
 }
 .allDiv div{
   display: flex;
@@ -128,5 +198,29 @@ a{
 }
 .allDiv div button{
   margin-top:1vmin;
+}
+h2{
+  font-family: 'Didact Gothic', sans-serif;
+  font-family: 'Jost', sans-serif;
+}
+h3{
+  font-family: 'Didact Gothic', sans-serif;
+  font-family: 'Jost', sans-serif;
+}
+button{
+  font-family: 'Didact Gothic', sans-serif;
+  font-family: 'Jost', sans-serif;
+}
+main{
+  display: flex;
+  flex-direction: row;
+}
+main div{
+  background-color: aqua;
+  display: flex;
+  flex-direction: column;
+}
+.errorPlace{
+  color: red;
 }
 </style>
